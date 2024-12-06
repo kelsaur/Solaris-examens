086040bf0edc4fa83ae033a5482f878bff4b2467
kelsaur <kellyb.karner@gmail.com>
2024-12-06 10:22:49 +0100

    examensarbete

--- script.js
+++ script.js
@@ -1,80 +1,118 @@
-const solen = document.querySelector('.Solen')
-const merkurius = document.querySelector('.Merkurius')
-const venus = document.querySelector('.Venus')
-const jorden = document.querySelector('.Jorden')
-const mars = document.querySelector('.Mars')
-const jupiter = document.querySelector('.Jupiter')
-const saturnus = document.querySelector('.Saturnus')
-const uranus = document.querySelector('.Uranus')
-const neptunus = document.querySelector('.Neptunus')
+const solen = document.querySelector('.solen')
+const merkurius = document.querySelector('.merkurius')
+const venus = document.querySelector('.venus')
+const jorden = document.querySelector('.jorden')
+const mars = document.querySelector('.mars')
+const jupiter = document.querySelector('.jupiter')
+const saturnus = document.querySelector('.saturnus')
+const uranus = document.querySelector('.uranus')
+const neptunus = document.querySelector('.neptunus')
 
-const planetSections = document.querySelectorAll('.container section') //To loop through each section class
-const allPlanets = document.querySelector('.container') //To hide the planets altogether
-const planets = [solen, merkurius, venus, jorden, mars, jupiter, saturnus, uranus, neptunus] //To get info which planet is clicked on
+const planets = [solen, merkurius, venus, jorden, mars, jupiter, saturnus, uranus, neptunus]
+
 
 /*Fetch the API key*/
-/*async function fetchApiKey()*/
+/*async function fetchApiKey() - before making it an arrow func*/
 const fetchApiKey = async () => {
   const response = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys', {
     method: 'POST'
   })
 
-  const data = await response.json()
-  const apiKey = data.key
-  return apiKey
+  const data = await response.json() //Creates a promise
+  //console.log(data)
+  const apiKey = data.key //Saves the promise value in a variable
+  return apiKey //To be able to use it later in fetchPlanetData
 }
 
 /*Fetch Solar system information*/
-/*async function getSolarInformation (apiKey)*/
-const getSolarInfo = async (apiKey) => {
+/*async function fetchPlanetData (apiKey) - before making it an arrow func*/
+const fetchPlanetData = async (apiKey) => {
   const response = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
     method: 'GET',
     headers: {'x-zocom': apiKey}
   })
   
-  const data = await response.json()
-  const solarInfo = data.bodies
-  return solarInfo
+  const data = await response.json() //Creates a promise
+  //console.log(data)
+  const solarInfo = data.bodies //Saves the promise value in a valuable
+  return solarInfo //To be able to use it later in fetchAndDisplayPlanetData
 }
 
-// Function to display planet info based on the clicked planet class
-const displaySolarInfo = async (planetClass) => {
+
+//Display the fetched planets info
+const fetchAndDisplayPlanetData = async () => {
+  //Calling and saving the API key in a variable
   const apiKey = await fetchApiKey()
-  console.log(`The API key is: ${apiKey}`) // Log the API key after it is returned
-  
-  const solarInfo = await getSolarInfo(apiKey) //Fetched information
-  
-  for (let i = 0; i < solarInfo.length; i++) { //Loops through the whole API fetch object, returns each id as it's own object
-    const body = solarInfo[i]
-    console.log(body) //Writes out each planet as it's own object
-    //console.log(`ID: ${body.id}, Name: ${body.name}, Type: ${body.type}, Latin name: ${body.latinName}, Längd på dygn: ${body.rotation}, Omkrets: ${body.circumference}, Temperatur på dagen: ${body.temp.day}, Temperatur på natten: ${body.temp.night}, Distans från solen: ${body.distance}, Jorddygn runt solen: ${body.orbitalPeriod}, Beskrivning: ${body.desc}, Månar: ${body.moons}`)
-  
-  if(body.name.toLowerCase() === planetClass.toLowerCase()) {
-      document.querySelector('.planet-name').textContent = `${body.name}`
-      document.querySelector('.planet-latin-name').textContent = `${body.latinName}`
-      document.querySelector('.planet-description').textContent = `${body.desc}`
-      document.querySelector('.planet-circumference').textContent = `${body.circumference}`
-      document.querySelector('.planet-distance').textContent = `${body.distance}`
-      document.querySelector('.planet-max-temp').textContent = `${body.temp.day}`
-      document.querySelector('.planet-min-temp').textContent = `${body.temp.night}`
-      document.querySelector('.planet-moons').textContent = `${body.moons}`
-      
-  }
 
+  //Calling and saving the planets info using the API key in a variable
+  let planetData = await fetchPlanetData(apiKey)
+
+  //Loop through the fetched data, since both fetched data and array of planets have just as many indexes, only one loop is needed
+  for (let i = 0; i < planetData.length; i++) {
+    
+    const currentPlanetData = planetData[i]
+    const currentPlanet = planets[i]
+    //console.log(currentPlanetData) - See fetched data names
+
+
+    //If indexes match print out this to querySelected classes in HTML
+    if (currentPlanet) {
+
+      currentPlanet.addEventListener('click', () => {
+      document.querySelector('.planet-name').textContent = `${currentPlanetData.name}`
+      document.querySelector('.planet-latin-name').textContent = `${currentPlanetData.latinName}`
+      document.querySelector('.planet-description').textContent = `${currentPlanetData.desc}`
+      document.querySelector('.planet-circumference').textContent = `${currentPlanetData.circumference} KM`
+      document.querySelector('.planet-distance').textContent = `${currentPlanetData.distance} KM`
+      document.querySelector('.planet-max-temp').textContent = `${currentPlanetData.temp.day} C`
+      document.querySelector('.planet-min-temp').textContent = `${currentPlanetData.temp.night} C`
+      document.querySelector('.planet-moons').textContent = `${currentPlanetData.moons.join(', ')}`
+
+
+      //If the planet has no moons print out this
+      if (currentPlanetData.moons.length === 0) {
+        document.querySelector('.planet-moons').textContent = `${currentPlanetData.name} har inga månar`
+    }
+
+    //Decide which CSS class is displayed based on te chosen planet index
+      let planetImage = document.querySelector('.planet')
+
+      if( i === 0) {
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('solen-clicked')
+      } else if (i === 1){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('merkurius-clicked')
+      } else if (i === 2){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('venus-clicked')
+      } else if (i === 3){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('jorden-clicked')
+      } else if (i === 4){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('mars-clicked')
+      } else if (i === 5){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('jupiter-clicked')
+      } else if (i === 6){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('saturnus-clicked')
+      } else if (i === 7){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('uranus-clicked')
+      } else if (i === 8){
+        planetImage.classList.remove('planet')
+        planetImage.classList.add('neptunus-clicked')
+      }
+
+      //Change visibility to planet-container when a planet is clicked
+      document.querySelector('.container').style.display = 'none'
+      document.querySelector('.planet-container').style.display = 'flex'
+    })
+    }
   }
-  return solarInfo
 }
 
-
-//When clicked on a planet, the rest of the planets are hidden and only chosen planet is shown.
-for(let i = 0; i < planetSections.length; i++) {
-planetSections[i].addEventListener('click', () => {
-  const planetName = planets[i].className
-  console.log(`Planet clicked is: ${planetName}`)
-  displaySolarInfo(planetName)
-
-
-  allPlanets.style.display = 'none'
-  document.querySelector('.planet-container').style.display = 'flex'
-})
-}
+//Call the function for everything to work
+fetchAndDisplayPlanetData()
